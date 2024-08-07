65b95a31284a9a8494ff0ccdd2bd3f4feeb3a7b9
junpeng <junpeng.hu@tum.de>
2024-08-07 16:55:16 +0200

    update

--- predict_single.py
+++ predict_single.py
@@ -75,53 +75,6 @@ class EstimatorInterface:
 
         return pose_pr
 
-
-def main(args):
-    cfg = load_cfg(args.cfg)
-    ref_database = parse_database_name(args.database)
-    estimator = name2estimator[cfg['type']](cfg)
-    estimator.build(ref_database, split_type='all')
-
-    object_pts = get_ref_point_cloud(ref_database)
-    object_bbox_3d = pts_range_to_bbox_pts(np.max(object_pts,0), np.min(object_pts,0))
-
-    output_dir = Path(args.output)
-    output_dir.mkdir(exist_ok=True, parents=True)
-
-    (output_dir / 'images_raw').mkdir(exist_ok=True, parents=True)
-    (output_dir / 'images_out').mkdir(exist_ok=True, parents=True)
-    (output_dir / 'images_inter').mkdir(exist_ok=True, parents=True)
-    (output_dir / 'images_out_smooth').mkdir(exist_ok=True, parents=True)
-
-    que_num = video2image(args.video, output_dir/'images_raw', 1, args.resolution, args.transpose)
-
-    pose_init = None
-    hist_pts = []
-    for que_id in tqdm(range(que_num)):
-        img = imread(str(output_dir/'images_raw'/f'frame{que_id}.jpg'))
-        # generate a pseudo K
-        h, w, _ = img.shape
-        f=np.sqrt(h**2+w**2)
-        K = np.asarray([[f,0,w/2],[0,f,h/2],[0,0,1]],np.float32)
-
-        if pose_init is not None:
-            estimator.cfg['refine_iter'] = 1 # we only refine one time after initialization
-        pose_pr, inter_results = estimator.predict(img, K, pose_init=pose_init) # 输出为当前相机位姿，世界到相机（在当前虚假K下，尺度为scale后的物体点云的尺度）
-        pose_init = pose_pr
-
-        pts, _ = project_points(object_bbox_3d, pose_pr, K) # 世界(点云文件)坐标系下的bbox，pose_pr: 世界到相机
-        bbox_img = draw_bbox_3d(img, pts, (0,0,255))
-        imsave(f'{str(output_dir)}/images_out/{que_id}-bbox.jpg', bbox_img)
-        np.save(f'{str(output_dir)}/images_out/{que_id}-pose.npy', pose_pr)
-        imsave(f'{str(output_dir)}/images_inter/{que_id}.jpg', visualize_intermediate_results(img, K, inter_results, estimator.ref_info, object_bbox_3d))
-
-        hist_pts.append(pts)
-        pts_ = weighted_pts(hist_pts, weight_num=args.num, std_inv=args.std) # 对box点云历史加权平均，得到smooth输出
-        pose_ = pnp(object_bbox_3d, pts_, K)
-        pts__, _ = project_points(object_bbox_3d, pose_, K)
-        bbox_img_ = draw_bbox_3d(img, pts__, (0,0,255))
-        imsave(f'{str(output_dir)}/images_out_smooth/{que_id}-bbox.jpg', bbox_img_)
-
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, default='configs/gen6d_pretrain_singleframe.yaml')
